08e509e7c40737e39ae9959a714a7fecbe409b64
mihail <mihail.zerket@gmail.com>
2021-11-15 09:10:01 +0100

    added validation model tests

---
+++ Reports.Infrastructure/Validators/ReportModelValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Reports.Domain.Models;
+using System;
+
+namespace Reports.Infrastructure.Validators
+{
+    public class ReportModelValidator : AbstractValidator<Report>
+    {
+        public ReportModelValidator()
+        {
+            RuleFor(model => model.CompanyId).NotEmpty().GreaterThan(0);
+            RuleFor(model => model.CompanyName).NotEmpty();
+
+            RuleFor(model => model.TrackerName).NotEmpty();
+
+            RuleFor(model => model.LastCrumbDtm).LessThan(DateTime.Now);
+            RuleFor(model => model.FirstCrumbDtm).LessThan(DateTime.Now);
+
+            RuleFor(model => model.AvgHumdity).GreaterThan(0).LessThanOrEqualTo(100);
+            RuleFor(model => model.AvgTemp).LessThan(1000);
+        }
+    }
+}
---
+++ Reports.Tests/ReportModelsValidationTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation.TestHelper;
+using NUnit.Framework;
+using Reports.Domain.Models;
+using Reports.Infrastructure.Validators;
+using System.Threading.Tasks;
+
+namespace Reports.Tests
+{
+    [TestFixture]
+    public class ReportModelsValidationTests
+    {
+        [Test]
+        public void Validate_ModelEmpty_Throws()
+        {
+            // Arrange
+            var model = new Report();
+            var sut = new ReportModelValidator();
+
+            // Act
+            var actual = sut.TestValidate(model);
+
+            actual.ShouldHaveValidationErrorFor(x => x.CompanyId);
+            actual.ShouldHaveValidationErrorFor(x => x.CompanyName);
+            actual.ShouldHaveValidationErrorFor(x => x.TrackerName);
+        }
+
+        [Test]
+        public void Validate_InvalidHumidity_Throws()
+        {
+            // Arrange
+            var model = new Report()
+            {
+                AvgHumdity = 0,
+                CompanyId = 1,
+                CompanyName = "New",
+                TrackerName =  "Tracker"
+            };
+            var sut = new ReportModelValidator();
+
+            // Act
+            var actual = sut.TestValidate(model);
+
+            actual.ShouldNotHaveValidationErrorFor(x => x.CompanyId);
+            actual.ShouldNotHaveValidationErrorFor(x => x.CompanyName);
+            actual.ShouldNotHaveValidationErrorFor(x => x.TrackerName);
+            actual.ShouldHaveValidationErrorFor(x => x.AvgHumdity);
+        }
+    }
+}
